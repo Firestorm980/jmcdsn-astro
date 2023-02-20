052aaec4a8d200477595df4ac36c73f39b7bfa3c
Jon Christensen <jon@jmchristensendesign.com>
2023-02-19 21:20:23 -0600

    Add integration for images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config'
+import image from '@astrojs/image'
 
-export default {
+// https://astro.build/config
+export default defineConfig({
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  site: 'https://firestorm980.github.io', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  site: 'https://firestorm980.github.io',
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   base: '/jmcdsn-astro',
-  integrations: [sitemap(), mdx()]
-}
+  integrations: [sitemap(), mdx(), image()]
+})
