9423f6b974326c56ce56608a9fe1a0c4a62d4901
Jon Christensen <jon@jmchristensendesign.com>
2023-09-06 16:58:43 -0500

    Update mdx and sitemap packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
 import { defineConfig } from 'astro/config'
-import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
