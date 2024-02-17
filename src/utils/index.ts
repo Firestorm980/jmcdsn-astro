d635543d0b8b37928c0e0042326b681569db8450
Jon Christensen <jon@jmchristensendesign.com>
2024-02-17 13:36:55 -0600

    Add utils

---
+++ src/utils/index.ts
@@ -0,0 +1,33 @@
+import type { Entry } from "@/types";
+
+export function sortEntriesByDate(entries: Entry[]) {
+    return entries.sort((a, b) => {
+        return new Date(b.data.date).getTime() - new Date(a.data.date).getTime();
+    });
+}
+
+export function sortEntriesByTitle(entries: Entry[]) {
+    return entries.sort((a, b) => {
+        return a.data.title.localeCompare(b.data.title);
+    });
+}
+
+export function filterDrafts(entries: Entry[]) {
+    return entries.filter(entry => {
+        return import.meta.env.PROD ? entry.data.draft !== true : true;
+    });
+}
+
+export function filterByCategory(entries: Entry[], category: string) {
+    return entries.filter(entry => {
+        if ( !entry.data.categories ) return false;
+        return entry.data.categories.includes(category)
+    });
+}
+
+export function filterByTag(entries: Entry[], tag: string) {
+    return entries.filter(entry => {
+        if ( !entry.data.tags ) return false;
+        return entry.data.tags.includes(tag)
+    });
+}
