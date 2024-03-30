a02533b29cdacf9596c6626c93f506c68c96278e
Jon Christensen <jon@jmchristensendesign.com>
2024-03-30 11:47:41 -0500

    Fix for head meta

--- src/assets/js/transition.js
+++ src/assets/js/transition.js
@@ -4,6 +4,7 @@ import SwupBodyClassPlugin from '@swup/body-class-plugin'
 import SwupProgressPlugin from '@swup/progress-plugin'
 import SwupScrollPlugin from '@swup/scroll-plugin'
 import SwupJsPlugin from '@swup/js-plugin'
+import SwupHeadPlugin from '@swup/head-plugin';
 import { close } from './base-header'
 
 /**
@@ -85,7 +86,8 @@ const init = () => {
       new SwupBodyClassPlugin(),
       new SwupProgressPlugin(),
       new SwupScrollPlugin(),
-      new SwupJsPlugin(options)
+      new SwupJsPlugin(options),
+      new SwupHeadPlugin()
     ]
   })
 }
